34711cbbc7a09281cebb17f2f06e703e7bbc3655
Gary Burgess <gary.burgess@gmail.com>
2017-04-18 07:37:52 +0100

    upgrate for PureScript 0.11.4 (#46)
    Release notes: https://github.com/purescript/purescript/releases/tag/v0.11.4

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ const test = require('tape');
 
 const {bin} = require('./package.json');
 const {SOURCE_URL} = require('./lib');
-const VERSION = '0.11.3';
+const VERSION = '0.11.4';
 const allowDifferentUserFlag = ' --allow-different-user'.repeat(Number(process.platform !== 'win32'));
 
 test('The package entry point', t => {
